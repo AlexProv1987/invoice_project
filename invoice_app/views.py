8bf5f850d52c0ccdf52c842d1d1c0394da06fe80
AlexProv1987 <alex.provenzano87@gmail.com>
2022-12-02 15:22:52 -0600

    First iteration of invoice form

--- invoice_app/views.py
+++ invoice_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .modelforms import invoiceform, lineitemformset
 # Create your views here.
+
+def invoicegen(request):
+    if request.method == 'POST':
+        print(request.POST)
+    invoice = invoiceform()
+    lineitems = lineitemformset()
+    context = {'invoice': invoice,'lineitems':lineitems}
+    return render(request, 'invoicegen.html', context)
