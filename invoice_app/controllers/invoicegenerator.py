e75353d1ec8e7a53833e8427f9073ce73245099e
AlexProv1987 <alex.provenzano87@gmail.com>
2022-12-05 13:09:22 -0600

    Save point for PDF generation

--- invoice_app/controllers/invoicegenerator.py
+++ invoice_app/controllers/invoicegenerator.py
@@ -5,6 +5,7 @@ from product_app.models import product
 from business_app.models import business,client
 from invoicegenie.project_classes.helperclass import postreqhelpers
 from django.core.exceptions import FieldError
+from fpdf import FPDF
 '''class to handle post request and create invoice object and all related objects'''
 class handleinvoicegen():
 
@@ -12,6 +13,7 @@ class handleinvoicegen():
         self.invoice = postreqhelpers.parsepost(postdict=postreq,keys=('bus_reltn', 'client_reltn'))
         self.postrequest = postreq
         self.invobj = invoice.objects.none()
+        self.lineitemobj = []
         self.issuccess = False
         if self._createinvoiceobj():
             if self._createlineitems():
@@ -54,6 +56,7 @@ class handleinvoicegen():
         
         if(len(lineitem_objects) != 0):
             lineitem.objects.bulk_create(lineitem_objects)
+            self.lineitemobj = lineitem_objects
             return True
 
 
@@ -71,8 +74,44 @@ class handleinvoicegen():
             totalBilled+=product_total
         return totalBilled
 
-    def _createinvoicepdf(self):
+#we have to inherit the base FPDF class to create our own header and footer methods as the default return none
+class myFPDF(FPDF):
+
+    def header(self) -> None:
         pass
+    def footer(self) -> None:
+        pass
+
+class generatepdf():
+
+    def __init__(self, liobj, invobj):
+        self.inv = invobj
+        self.lineitems = liobj
+        self.pdffile = f'{self.inv.pk}.pdf'
+        self._pdfgeneration()
+
+    def _pdfgeneration(self):
+        pdf = myFPDF('P', 'mm', 'A4')
+        pdf.add_page()
+        pdf.set_font('courier', 'B', 12)
+        pdf.cell(40,10, f'Invoice#{self.inv.pk}'.rjust(40),0,1)
+        pdf.cell(200,10, f"{'Pay To:'.ljust(35)} {'Client:'.rjust(20)}",0,1)
+        pdf.set_font('courier', '', 12)
+        pdf.cell(200,10,f"{str(self.inv.bus_reltn.bus_name).ljust(35)} {str(self.inv.client_reltn.client_name).rjust(24)}",0,1)
+        pdf.cell(200,10,f"{str(self.inv.bus_reltn.bus_street).ljust(35)} {str(self.inv.client_reltn.client_street).rjust(24)}",0,1)
+        pdf.cell(200,10,f"{str(self.inv.bus_reltn.bus_zip).ljust(35)} {str(self.inv.client_reltn.client_zip).rjust(24)}",0,1)
+        pdf.cell(200,10,f"{str(self.inv.bus_reltn.bus_phone).ljust(35)} {str(self.inv.client_reltn.client_phone).rjust(24)}",0,1)
+        pdf.cell(200,10,f"{str(self.inv.bus_reltn.bus_email).ljust(35)} {str(self.inv.client_reltn.client_email).rjust(24)}",0,1)
+        pdf.ln(10)
+        pdf.set_font('courier', '', 12)
+        pdf.cell(200, 8, f"{'Item'.ljust(30)} {'Qty'.rjust(20)} {'Total'.rjust(10)}", 0, 1)
+        for li in self.lineitems:
+            pdf.cell(200, 8, f"{str(li.product.p_name).ljust(30)} {str(li.line_item_qty).rjust(20)} {str(li.line_item_amt).rjust(10)}" , 0, 1)
+        pdf.ln(10)
+        pdf.cell(200,8,f"{''.ljust(30)} {'Qty Total'.rjust(25)} {'Total Due'.rjust(10)}", 0, 1)
+        pdf.cell(200, 8, f"{''.ljust(30)} {str(self.inv.product_qty).rjust(20)} {str(self.inv.total_billed).rjust(15)}" , 0, 1)
+        pdf.output(self.pdffile, 'F')
+
 '''
 EX postreq
 <QueryDict: 
@@ -90,6 +129,4 @@ EX postreq
 'form-4-product': ['1'], 'form-4-line_item_qty': ['1'], 'form-4-id': [''], 
 'form-5-product': ['1'], 'form-5-line_item_qty': ['1'], 'form-5-id': ['']
 }
->
-
 '''
--- invoice_app/views.py
+++ invoice_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render,redirect
 from .modelforms import invoiceform, lineitemformset
-from .controllers.invoicegenerator import handleinvoicegen
+from .controllers.invoicegenerator import handleinvoicegen, generatepdf
+from fpdf import FPDF
+from django.http import FileResponse
 # Create your views here.
 
+#view method will be split later, just sticking to one for testing
 def invoicegen(request):
     if request.method == 'POST':
-        handleinvoicegen(request.POST)
+        newinv = handleinvoicegen(request.POST)
+        invpdf = generatepdf(newinv.lineitemobj,newinv.invobj)
         '''Test Code'''
+        return FileResponse(open(invpdf.pdffile, 'rb'), as_attachment=True, content_type='application/pdf')
         '''End Test code'''
     invoice = invoiceform()
     lineitems = lineitemformset()
