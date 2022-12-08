db99c5b9b10d1574e1f56d095258b4ec99c1b218
AlexProv1987 <alex.provenzano87@gmail.com>
2022-12-08 11:37:41 -0600

    Changed to staticmethods

--- invoicegenie/project_classes/helperclass.py
+++ invoicegenie/project_classes/helperclass.py
@@ -1,8 +1,9 @@
+'''re usable class that holds static methods to parse a post request that can be called without constructing an instance of the class'''
 class postreqhelpers():
     def __init__(self) -> None:
-        pass   
-    @classmethod
-    def parsepost(self,postdict:dict, keys:tuple) -> dict:
+        pass
+    @staticmethod
+    def parsepost(postdict:dict, keys:tuple) -> dict:
         returnDict = {}
         #check type
         if not isinstance(postdict,dict):
@@ -16,8 +17,21 @@ class postreqhelpers():
         print(returnDict)
         return returnDict
 
+'''unused'''
 class datehelpers():
     pass
 
+'''unused'''
 class mathhelpers():
     pass
+
+'''class containing static methods to be called to assist in validating data sent to the server'''
+class postreqvalidator():
+    def __init__(self) -> None:
+        pass
+    @staticmethod
+    def checktype():
+        pass
+    @staticmethod
+    def checkkeys():
+        pass
