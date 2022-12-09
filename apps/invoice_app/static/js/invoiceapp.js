ddf3f5a56c84c439485f885df3194fd8bdd64c38
AlexProv1987 <alex.provenzano87@gmail.com>
2022-12-09 17:45:47 -0600

    Invoice Status Update

--- apps/invoice_app/static/js/invoiceapp.js
+++ apps/invoice_app/static/js/invoiceapp.js
@@ -1,21 +1,22 @@
-
-let lineitemform = document.querySelectorAll(".lineitem")
-let container = document.querySelector("#form-container")
 let addButton = document.querySelector("#add-form")
-let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
-let formNum = lineitemform.length-1
+if(addButton)
+{
+    let lineitemform = document.querySelectorAll(".lineitem")
+    let container = document.querySelector("#form-container")
+    let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
+    let formNum = lineitemform.length-1
 
-addButton.addEventListener('click', addForm)
+    addButton.addEventListener('click', addForm)
+    function addForm(e){
+        e.preventDefault()
 
-function addForm(e){
-    e.preventDefault()
+        let newForm = lineitemform[0].cloneNode(true)
+        let formRegex = RegExp(`form-(\\d){1}-`,'g')
 
-    let newForm = lineitemform[0].cloneNode(true)
-    let formRegex = RegExp(`form-(\\d){1}-`,'g')
-
-    formNum++
-    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`)
-    container.insertBefore(newForm, addButton)
+        formNum++
+        newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`)
+        container.insertBefore(newForm, addButton)
     
-    totalForms.setAttribute('value', `${formNum+1}`)
+        totalForms.setAttribute('value', `${formNum+1}`)
+    }
 }
--- invoicegenie/project_classes/modelobjupdater.py
+++ invoicegenie/project_classes/modelobjupdater.py
@@ -1,3 +1,10 @@
+'''class accepts a db model object, and a dictionary of field(key),updatevalue(value) pairs
+'''
+
 class updatemodelobj():
     def __init__(self, modelobj, **kwargs) -> None:
+        self.obj = modelobj
+        self.keyval = kwargs
+
+    def _updateobj(self):
         pass
