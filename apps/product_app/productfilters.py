7fd225efe1915812eafc23795553b822e17b8de0
AlexProv1987 <alex.provenzano87@gmail.com>
2022-12-19 18:07:36 -0600

    Formatting

--- apps/product_app/productfilters.py
+++ apps/product_app/productfilters.py
@@ -2,7 +2,9 @@ from django_filters import FilterSet
 import django_filters as filters
 from .models import product
 class productfilter(FilterSet):
-    product_name = filters.CharFilter(field_name='p_name', lookup_expr='icontains', label='Product Name: ')
+    BOOLEAN_CHOICES = ((0, 'Out Of Stock'), (1, 'In Stock'),)
+    product_name = filters.CharFilter(field_name='p_name', lookup_expr='icontains', label='Product Name:')
+    in_stock = filters.ChoiceFilter(field_name='p_is_active',choices=BOOLEAN_CHOICES, label='In Stock:')
     class Meta:
         model = product
         fields = []
