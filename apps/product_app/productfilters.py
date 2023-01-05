9173da3f420682801e3c81e09096d356a3e28e9f
AlexProv1987 <alex.provenzano87@gmail.com>
2023-01-05 16:46:30 -0600

    Modifcations to Model fields for product

--- apps/product_app/productfilters.py
+++ apps/product_app/productfilters.py
@@ -2,9 +2,9 @@ from django_filters import FilterSet
 import django_filters as filters
 from .models import product
 class productfilter(FilterSet):
-    BOOLEAN_CHOICES = ((0, 'Out Of Stock'), (1, 'In Stock'),)
+    #BOOLEAN_CHOICES = ((0, 'Out Of Stock'), (1, 'In Stock'),)
     product_name = filters.CharFilter(field_name='p_name', lookup_expr='icontains', label='Product Name:')
-    in_stock = filters.ChoiceFilter(field_name='p_is_active',choices=BOOLEAN_CHOICES, label='In Stock:')
+    in_stock = filters.ChoiceFilter(field_name='p_instock', choices=product.stock_choices, label='In Stock:')
     class Meta:
         model = product
         fields = []
