5d3a38f565c9a543748482481fc59f636208d784
AlexProv1987 <alex.provenzano87@gmail.com>
2023-01-26 16:03:09 -0600

    dict not method

--- invoicegenie/settings.py
+++ invoicegenie/settings.py
@@ -153,8 +153,8 @@ LOGOUT_REDIRECT_URL = "/accounts/login"
 
 # Emailbackend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ('EMAIL_HOST')
+EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
