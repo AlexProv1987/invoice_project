a0bb755e57a0d04d3397b98aa027c2836a14df5d
AlexProv1987 <96451610+AlexProv1987@users.noreply.github.com>
2023-02-15 13:44:03 -0600

    Aprovbranch (#16)
    * Removed invoicefile model
    
    * Removed invoicefile from admin - tabled rmvd
    
    * removed local
    
    * bill_inv commit among others
    
    * Second iteration of email command
    
    * method rename to adhere to python standard
    
    * Payment posting logic updates
    
    * Settings for prod and local
    
    * link correction
    
    * Added inv# to payment form
    
    * Added inv to context variable
    
    * Settings
    
    * Font Size Changes
    
    * Font Size
    
    * header position change
    
    * position...
    
    * pos adjustment again
    
    * closer pos adjustment
    
    * still not inline
    
    * temp logo removed
    
    * POS Adjustments
    
    * POS Adjustments Again and Again
    
    * POS
    
    * POS Adjust
    
    * adjust
    
    * Adjustment to POS
    
    * Made the status check for payment less wordy
    
    * Table POS fix
    
    * Redirect to previous URL
    
    * Reverting previous redirect..ew.
    
    * Fixed incorrect view logic to display frm errors
    
    * Changed to positive int over big positive
    
    * Migrations
    
    * Filterview defect fix
    
    * Added space between city st
    
    * Added Index's for common searches
    
    * filter form spacing
    
    * Well Create Superuser via SSH
    
    * Table Fixes
    
    * Removed unneeded py files
    
    * Revert Test Settings
    
    * login required on class based views

--- apps/business_app/urls.py
+++ apps/business_app/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from .views import viewclients,updatebusiness,modifyclient, createclient, invoicedisplay,managebusiness, invbyclient
 urlpatterns = [
-    path('managebus/',managebusiness.as_view(), name='manage-bus'),
-    path('updatebusiness/<int:pk>/',updatebusiness.as_view(),name='update-business'),
-    path('updateclient/<int:pk>',modifyclient.as_view(),name='update-client'),
-    path('createclient/',createclient.as_view(),name='create-client'),
-    path('businvoices/',invoicedisplay.as_view(),name='view-invoices'),
-    path('invbyclient/<int:pk>',invbyclient.as_view(), name='inv-byclient'),
-    path('clientlist/', viewclients.as_view(), name='view-clients')
+    path('managebus/',login_required(managebusiness.as_view()), name='manage-bus'),
+    path('updatebusiness/<int:pk>/',login_required(updatebusiness.as_view()),name='update-business'),
+    path('updateclient/<int:pk>',login_required(modifyclient.as_view()),name='update-client'),
+    path('createclient/',login_required(createclient.as_view()),name='create-client'),
+    path('businvoices/',login_required(invoicedisplay.as_view()),name='view-invoices'),
+    path('invbyclient/<int:pk>',login_required(invbyclient.as_view()), name='inv-byclient'),
+    path('clientlist/', login_required(viewclients.as_view()), name='view-clients')
 ]
--- apps/payment_app/urls.py
+++ apps/payment_app/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from .views import makepayment
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
-    path('payment/<int:pk>',makepayment.as_view(), name='make-payment'),
+    path('payment/<int:pk>',login_required(makepayment.as_view()), name='make-payment'),
 ]
