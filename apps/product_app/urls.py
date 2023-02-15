34e18d4ccf9b22f28942e17db512d9ee0e8a8318
AlexProv1987 <alex.provenzano87@gmail.com>
2023-02-15 13:43:04 -0600

    login required on class based views

--- apps/product_app/urls.py
+++ apps/product_app/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
 from .views import addproduct, modifyproduct,productdisplay
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
-    path('update/<int:pk>/',modifyproduct.as_view(),name='update-product'),
-    path('addproduct/',addproduct.as_view(),name='add-product'),
-    path('viewproducts/',productdisplay.as_view(),name='view-products'),
+    path('update/<int:pk>/',login_required(modifyproduct.as_view()),name='update-product'),
+    path('addproduct/',login_required(addproduct.as_view()),name='add-product'),
+    path('viewproducts/',login_required(productdisplay.as_view()),name='view-products'),
 ]
